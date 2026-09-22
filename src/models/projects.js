de8b7322662197c91410d8dde5339caf3658ea44
Samizy29 <onisamuel73@gmail.com>
2026-09-22 14:24:29 -0700

    Add database model files for categories, organizations, and projects with query functions

---
+++ src/models/projects.js
@@ -0,0 +1,18 @@
+import db from './db.js'
+
+const getAllProjects = async () => {
+    const query = `
+        SELECT project.project_id, project.title, project.description,
+               project.location, project.project_date, project.organization_id,
+               organization.name AS organization_name
+        FROM public.project
+        JOIN public.organization ON project.organization_id = organization.organization_id
+        ORDER BY project.project_date;
+    `;
+
+    const result = await db.query(query);
+
+    return result.rows;
+}
+
+export { getAllProjects }
